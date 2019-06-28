19fd27a0287b29f0400d8e3be4f614cad26fb42f
therjak <therjak@gmx.net>
2019-06-28 20:51:25 +0200

    started implementing PR_Execute

--- quakelib/vm.go
+++ quakelib/vm.go
@@ -8,6 +8,7 @@ package quakelib
 import "C"
 
 import (
+	"log"
 	"quake/conlog"
 	"quake/progs"
 	"strings"
@@ -314,7 +315,7 @@ func (v *virtualMachine) enterFunction(f *progs.Function) int32 {
 	}
 
 	v.xfunction = f
-	return f.FirstStatement - 1 // offset the s++ -- THERJAK: What s++
+	return f.FirstStatement
 }
 
 func (v *virtualMachine) leaveFunction() int32 {
@@ -338,4 +339,333 @@ func (v *virtualMachine) leaveFunction() int32 {
 	v.stack = v.stack[:len(v.stack)-1]
 	v.xfunction = v.stack[len(v.stack)-1].function
 	return v.stack[len(v.stack)-1].statement
+}
+
+//  The interpretation main loop
+func (v *virtualMachine) ExecuteProgram(fnum int32) {
+	/*
+	  eval_t *ptr;
+	  dstatement_t *st;
+	  dfunction_t *f, *newf;
+	  int ed;
+	*/
+
+	if fnum == 0 || int(fnum) >= len(v.prog.Functions) {
+		if v.prog.Globals.Self != 0 {
+			edictPrint(int(v.prog.Globals.Self))
+		}
+		log.Printf("PR_ExecuteProgram %d", fnum)
+		HostError("PR_ExecuteProgram: NULL function")
+	}
+
+	f := &v.prog.Functions[fnum]
+
+	v.trace = false
+
+	// make a stack frame
+	exitdepth := len(v.stack)
+
+	currentStatement := v.enterFunction(f)
+
+	st := func() *progs.Statement {
+		return &v.prog.Statements[currentStatement]
+	}
+
+	OPAF := func() float32 {
+		return v.prog.RawGlobalsF[st().A]
+	}
+	OPBF := func() float32 {
+		return v.prog.RawGlobalsF[st().B]
+	}
+	setOPCF := func(X float32) {
+		v.prog.RawGlobalsF[st().C] = X
+	}
+
+	OPAI := func() int32 {
+		return v.prog.RawGlobalsI[st().A]
+	}
+	OPBI := func() int32 {
+		return v.prog.RawGlobalsI[st().B]
+	}
+	setOPBI := func(X int32) {
+		v.prog.RawGlobalsI[st().B] = X
+	}
+	//SOPCI := func(X int32) {
+	//	v.prog.RawGlobalsI[st().C] = X
+	//}
+
+	OPAV1 := func() float32 {
+		return v.prog.RawGlobalsF[st().A]
+	}
+	OPAV2 := func() float32 {
+		return v.prog.RawGlobalsF[st().A+1]
+	}
+	OPAV3 := func() float32 {
+		return v.prog.RawGlobalsF[st().A+2]
+	}
+	OPBV1 := func() float32 {
+		return v.prog.RawGlobalsF[st().B]
+	}
+	OPBV2 := func() float32 {
+		return v.prog.RawGlobalsF[st().B+1]
+	}
+	OPBV3 := func() float32 {
+		return v.prog.RawGlobalsF[st().B+2]
+	}
+	setOPBV1 := func(X float32) {
+		v.prog.RawGlobalsF[st().B] = X
+	}
+	setOPBV2 := func(X float32) {
+		v.prog.RawGlobalsF[st().B+1] = X
+	}
+	setOPBV3 := func(X float32) {
+		v.prog.RawGlobalsF[st().B+2] = X
+	}
+	setOPCV1 := func(X float32) {
+		v.prog.RawGlobalsF[st().C] = X
+	}
+	setOPCV2 := func(X float32) {
+		v.prog.RawGlobalsF[st().C+1] = X
+	}
+	setOPCV3 := func(X float32) {
+		v.prog.RawGlobalsF[st().C+2] = X
+	}
+
+	BOOL := func(X bool) float32 {
+		if X {
+			return 1
+		}
+		return 0
+	}
+
+	startprofile := int32(0)
+	profile := int32(0)
+
+	//hack to offset the first increment of currentStatement
+	currentStatement--
+	for {
+		currentStatement++
+		profile++
+		if profile > 100000 {
+			v.statement = currentStatement - int32(len(v.prog.Statements))
+			v.RunError("runaway loop error")
+		}
+
+		if v.trace {
+			v.printStatement(v.prog.Statements[currentStatement])
+		}
+
+		switch st().Operator {
+		case operatorADD_F:
+			setOPCF(OPAF() + OPBF())
+		case operatorADD_V:
+			setOPCV1(OPAV1() + OPBV1())
+			setOPCV2(OPAV2() + OPBV2())
+			setOPCV3(OPAV3() + OPBV3())
+
+		case operatorSUB_F:
+			setOPCF(OPAF() - OPBF())
+		case operatorSUB_V:
+			setOPCV1(OPAV1() - OPBV1())
+			setOPCV2(OPAV2() - OPBV2())
+			setOPCV3(OPAV3() - OPBV3())
+
+		case operatorMUL_F:
+			setOPCF(OPAF() * OPBF())
+		case operatorMUL_V:
+			setOPCF(
+				OPAV1()*OPBV1() +
+					OPAV2()*OPBV2() +
+					OPAV3()*OPBV3())
+		case operatorMUL_FV:
+			setOPCV1(OPAF() * OPBV1())
+			setOPCV2(OPAF() * OPBV2())
+			setOPCV3(OPAF() * OPBV3())
+		case operatorMUL_VF:
+			setOPCV1(OPBF() * OPAV1())
+			setOPCV2(OPBF() * OPAV2())
+			setOPCV3(OPBF() * OPAV3())
+
+		case operatorDIV_F:
+			setOPCF(OPAF() / OPBF())
+
+		case operatorBITAND:
+			// This hurts
+			r := (int(OPAF()) & int(OPBF()))
+			setOPCF(float32(r))
+		case operatorBITOR:
+			// This hurts
+			r := (int(OPAF()) | int(OPBF()))
+			setOPCF(float32(r))
+
+		case operatorGE:
+			setOPCF(BOOL(OPAF() >= OPBF()))
+		case operatorLE:
+			setOPCF(BOOL(OPAF() <= OPBF()))
+		case operatorGT:
+			setOPCF(BOOL(OPAF() > OPBF()))
+		case operatorLT:
+			setOPCF(BOOL(OPAF() < OPBF()))
+		case operatorAND:
+			setOPCF(BOOL((OPAF() != 0) && (OPBF() != 0)))
+		case operatorOR:
+			setOPCF(BOOL((OPAF() != 0) || (OPBF() != 0)))
+
+		case operatorNOT_F:
+			setOPCF(BOOL(OPAF() == 0))
+		case operatorNOT_V:
+			setOPCF(BOOL(
+				(OPAV1() == 0) &&
+					(OPAV2() == 0) &&
+					(OPAV3() == 0)))
+			/*
+			   case OP_NOT_S:
+			     setOPCF(!OPAI || !*PR_GetString(OPAI));
+			*/
+		case operatorNOT_FNC:
+			setOPCF(BOOL(OPAI() == 0))
+		case operatorNOT_ENT:
+			setOPCF(BOOL(OPAI() == 0))
+
+		case operatorEQ_F:
+			setOPCF(BOOL(OPAF() == OPBF()))
+		case operatorEQ_V:
+			setOPCF(BOOL(
+				(OPAV1() == OPBV1()) &&
+					(OPAV2() == OPBV2()) &&
+					(OPAV3() == OPBV3())))
+			/*
+			   case OP_EQ_S:
+			     setOPCF(!strcmp(PR_GetString(OPAI), PR_GetString(OPBI)));
+			*/
+		case operatorEQ_E:
+			setOPCF(BOOL(OPAI() == OPBI()))
+		case operatorEQ_FNC:
+			setOPCF(BOOL(OPAI() == OPBI()))
+		case operatorNE_F:
+			setOPCF(BOOL(OPAF() != OPBF()))
+		case operatorNE_V:
+			setOPCF(BOOL(
+				(OPAV1() != OPBV1()) ||
+					(OPAV2() != OPBV2()) ||
+					(OPAV3() != OPBV3())))
+			/*
+			   case operatorNE_S:
+			     SOPCF(strcmp(PR_GetString(OPAI), PR_GetString(OPBI)));
+			*/
+		case operatorNE_E:
+			setOPCF(BOOL(OPAI() != OPBI()))
+		case operatorNE_FNC:
+			setOPCF(BOOL(OPAI() != OPBI()))
+
+		case operatorSTORE_F,
+			operatorSTORE_ENT,
+			operatorSTORE_FLD, // integers
+			operatorSTORE_S,
+			operatorSTORE_FNC: // pointers
+			setOPBI(OPAI())
+		case operatorSTORE_V:
+			setOPBV1(OPAV1())
+			setOPBV2(OPAV2())
+			setOPBV3(OPAV3())
+
+			/*
+			   case OP_STOREP_F:
+			   case OP_STOREP_ENT:
+			   case OP_STOREP_FLD:  // integers
+			   case OP_STOREP_S:
+			   case OP_STOREP_FNC:  // pointers
+			     ptr = (eval_t *)((byte *)EVars(0) + OPBI);
+			     ptr->_int = OPAI;
+			     break;
+			   case OP_STOREP_V:
+			     ptr = (eval_t *)((byte *)EVars(0) + OPBI);
+			     ptr->vector[0] = OPAV1;
+			     ptr->vector[1] = OPAV2;
+			     ptr->vector[2] = OPAV3;
+			     break;
+
+			   case OP_ADDRESS:
+			     ed = OPAI;
+			     if (ed == 0 && SV_State() == ss_active) {
+			       pr_xstatement = st - pr_statements;
+			       PR_RunError("assignment to world entity");
+			     }
+			     SOPCI((byte *)((int *)EVars(OPAI) + OPBI) - (byte *)EVars(0));
+			     break;
+
+			   case OP_LOAD_F:
+			   case OP_LOAD_FLD:
+			   case OP_LOAD_ENT:
+			   case OP_LOAD_S:
+			   case OP_LOAD_FNC:
+			     SOPCI(((eval_t *)((int *)EVars(OPAI) + OPBI))->_int);
+			     break;
+
+			   case OP_LOAD_V:
+			     ptr = (eval_t *)((int *)EVars(OPAI) + OPBI);
+			     SOPCV1(ptr->vector[0]);
+			     SOPCV2(ptr->vector[1]);
+			     SOPCV3(ptr->vector[2]);
+			     break;
+			*/
+		case operatorIFNOT:
+			if OPAI() == 0 {
+				currentStatement += int32(st().B) - 1 // -1 to offset the st++
+			}
+
+		case operatorIF:
+			if OPAI() != 0 {
+				currentStatement += int32(st().B) - 1 // -1 to offset the st++
+			}
+
+		case operatorGOTO:
+			currentStatement += int32(st().A) - 1 // -1 to offset the st++
+			/*
+			   case OP_CALL0:
+			   case OP_CALL1:
+			   case OP_CALL2:
+			   case OP_CALL3:
+			   case OP_CALL4:
+			   case OP_CALL5:
+			   case OP_CALL6:
+			   case OP_CALL7:
+			   case OP_CALL8:
+			     pr_xfunction->profile += profile - startprofile;
+			     startprofile = profile;
+			     pr_xstatement = st - pr_statements;
+			     pr_argc = st->op - OP_CALL0;
+			     if (!OPAI) PR_RunError("NULL function");
+			     newf = &pr_functions[OPAI];
+			     if (newf->first_statement < 0) {  // Built-in function
+			       int i = -newf->first_statement;
+			       if (i >= pr_numbuiltins) PR_RunError("Bad builtin call number %d", i);
+			       pr_builtins[i]();
+			       break;
+			     }
+			     // Normal function
+			     st = &pr_statements[PR_EnterFunction(newf)];
+			     break;
+			*/
+		case operatorDONE, operatorRETURN:
+			v.xfunction.Profile += profile - startprofile
+			startprofile = profile
+			v.statement = currentStatement - int32(len(v.prog.Statements))
+			*(v.prog.Globals.Returnf()) = [3]float32{OPAV1(), OPAV2(), OPAV3()}
+			currentStatement = v.leaveFunction()
+			if len(v.stack) == exitdepth { // Done
+				return
+			}
+		case operatorSTATE:
+			/*
+			   EVars(Pr_global_struct_self())->nextthink =
+			       Pr_global_struct_time() + 0.1;
+			   EVars(Pr_global_struct_self())->frame = OPAF;
+			   EVars(Pr_global_struct_self())->think = OPBI;
+			*/
+		default:
+			v.statement = currentStatement - int32(len(v.prog.Statements))
+			v.RunError("Bad opcode %d", v.prog.Statements[currentStatement].Operator)
+		}
+	}
 }
