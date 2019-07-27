e02d4014e608de925046ee4aac92135eac31d8be
therjak <therjak@gmx.net>
2019-07-27 09:23:51 +0200

    init some keys

--- quakelib/keys.go
+++ quakelib/keys.go
@@ -21,11 +21,40 @@ var (
 	keydown   map[kc.KeyCode]bool
 )
 
+func init() {
+	consolekeys = make(map[kc.KeyCode]bool)
+	menubound = make(map[kc.KeyCode]bool)
+	keydown = make(map[kc.KeyCode]bool)
+
+	for i := 32; i < 127; i++ {
+		// ascii characters
+		consolekeys[kc.KeyCode(i)] = true
+	}
+	for _, k := range []kc.KeyCode{kc.TAB, kc.ENTER, kc.ESCAPE, kc.BACKSPACE,
+		kc.UPARROW, kc.DOWNARROW, kc.LEFTARROW, kc.RIGHTARROW,
+		kc.CTRL, kc.SHIFT,
+		kc.INS, kc.DEL, kc.PGDN, kc.PGUP, kc.HOME, kc.END,
+		kc.KP_NUMLOCK, kc.KP_SLASH, kc.KP_STAR, kc.KP_MINUS, kc.KP_HOME,
+		kc.KP_UPARROW, kc.KP_PGUP, kc.KP_PLUS,
+		kc.KP_LEFTARROW, kc.KP_5, kc.KP_RIGHTARROW,
+		kc.KP_END, kc.KP_DOWNARROW, kc.KP_PGDN,
+		kc.KP_ENTER, kc.KP_INS, kc.KP_DEL,
+		kc.MWHEELUP, kc.MWHEELDOWN} {
+		consolekeys[k] = true
+	}
+	// only on MAC?
+	// consolekeys[K_COMMAND] = true;
+
+	for _, k := range []kc.KeyCode{kc.ESCAPE,
+		kc.F1, kc.F2, kc.F3, kc.F4, kc.F5,
+		kc.F6, kc.F7, kc.F8, kc.F9, kc.F10,
+		kc.F11, kc.F12} {
+		menubound[k] = true
+	}
+}
+
 /*
 char *keybindings[MAX_KEYS];
-qboolean consolekeys[MAX_KEYS];  // if true, can't be rebound while in console
-qboolean menubound[MAX_KEYS];    // if true, can't be rebound while in menu
-qboolean keydown[MAX_KEYS];
 */
 
 //export GetKeyDest
