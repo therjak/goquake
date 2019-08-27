3c27fad059b52d23ee4654dfae769498d14c662d
therjak <therjak@gmx.net>
2019-08-27 19:03:11 +0200

    some prep for texture.go

--- quakelib/texture.go
+++ quakelib/texture.go
@@ -5,3 +5,13 @@ package quakelib
 import "C"
 
 type Texture C.gltexture_t
+
+// gl.TEXTURE0 == GL_TEXTURE0_ARB
+// gl.TEXTURE1 == GL_TEXTURE1_ARB
+// gl.TEXTURE_2D == GL_TEXTURE_2D
+// GL_UNUSED_TEXTURE is quake specific "^uint32(0)"
+//
+// gl.ActiveTexture == GL_SelectTextureFunc
+// gl.BindTexture == glBindTexture
+// gl.Enable == glEnable
+// gl.Disable == glDisable
