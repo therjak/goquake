b1d394c460a5ffc1aff20a724d31ccbe8a4e83aa
therjak <therjak@gmx.net>
2020-05-22 09:54:09 +0200

    let go-sdl handle sdl linker settings

--- quakelib/build.go
+++ quakelib/build.go
@@ -1,7 +1,7 @@
 package quakelib
 
 /*
-#cgo linux LDFLAGS: -lm -lGL -lSDL2
-#cgo linux CFLAGS: -I /usr/include/SDL2 -Wno-incompatible-pointer-types-discards-qualifiers -Wno-discarded-qualifiers -Wno-unknown-warning-option
+#cgo linux LDFLAGS: -lm -lGL
+#cgo linux CFLAGS: -Wno-incompatible-pointer-types-discards-qualifiers -Wno-discarded-qualifiers -Wno-unknown-warning-option
 */
 import "C"
