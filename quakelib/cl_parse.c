therjak/goquake

commit b59eb2dcf911508ccbf5529e345ff503c4d6b201

convert CL_ParseBaseline to go

quakelib/cl_parse.c
@@ -435,7 +435,7 @@ void CL_ParseUpdate(int bits) {
   if (bits & U_EFFECTS)
     ent->effects = CL_MSG_ReadByte();
   else
-    ent->effects = ent->baseline.effects;
+    ent->effects = 0;
 
   // shift the known values for interpolation
   VectorCopy(ent->msg_origins[0], ent->msg_origins[1]);
@@ -540,36 +540,6 @@ void CL_ParseUpdate(int bits) {
 }
 
 /*
-==================
-CL_ParseBaseline
-==================
-*/
-void CL_ParseBaseline(entity_t *ent, int version)  // johnfitz -- added argument
-{
-  int i;
-  int bits;  // johnfitz
-
-  // johnfitz -- PROTOCOL_FITZQUAKE
-  bits = (version == 2) ? CL_MSG_ReadByte() : 0;
-  ent->baseline.modelindex =
-      (bits & B_LARGEMODEL) ? CL_MSG_ReadShort() : CL_MSG_ReadByte();
-  ent->baseline.frame =
-      (bits & B_LARGEFRAME) ? CL_MSG_ReadShort() : CL_MSG_ReadByte();
-  // johnfitz
-
-  CL_MSG_ReadByte();  // colormap -- no idea what this is good for
-  ent->baseline.skin = CL_MSG_ReadByte();
-  for (i = 0; i < 3; i++) {
-    ent->baseline.origin[i] = CL_MSG_ReadCoord();
-    ent->baseline.angles[i] = CL_MSG_ReadAngle(CL_ProtocolFlags());
-  }
-
-  ent->baseline.alpha =
-      (bits & B_ALPHA) ? CL_MSG_ReadByte()
-                       : ENTALPHA_DEFAULT;  // johnfitz -- PROTOCOL_FITZQUAKE
-}
-
-/*
 =====================
 CL_NewTranslation
 =====================
@@ -598,7 +568,7 @@ void CL_ParseStatic(int version)  // johnfitz -- added a parameter
 
   ent = &cl_static_entities[i];
   Inc_CL_num_statics();
-  CL_ParseBaseline(ent, version);  // johnfitz -- added second parameter
+  CL_ParseBaseline(i, version);  // johnfitz -- added second parameter
 
   // copy it to the current state
 
@@ -607,7 +577,7 @@ void CL_ParseStatic(int version)  // johnfitz -- added a parameter
   ent->frame = ent->baseline.frame;
 
   ent->skinnum = ent->baseline.skin;
-  ent->effects = ent->baseline.effects;
+  ent->effects = 0;
   ent->alpha = ent->baseline.alpha;  // johnfitz -- alpha
 
   VectorCopy(ent->baseline.origin, ent->origin);

quakelib/client_parse.go
@@ -6,7 +6,6 @@ package quakelib
 //void CL_ParseStatic(int version);
 //struct entity_s;
 //struct entity_s* CL_EntityNum(int num);
-//void CL_ParseBaseline(struct entity_s *ent, int version);
 //void R_CheckEfrags(void);
 //void Fog_Update(float density, float red, float green, float blue, float time);
 import "C"
@@ -75,6 +74,106 @@ var (
 		"",                      // 49
 	}
 )
+
+//export CL_ParseBaseline
+func CL_ParseBaseline(i, version int) {
+	var err error
+	e := CL_EntityNum(i)
+	es := &EntityState{
+		Alpha: svc.EntityAlphaDefault,
+	}
+	bits := byte(0)
+	if version == 2 {
+		bits, err = cls.inMessage.ReadByte()
+		if err != nil {
+			cls.msgBadRead = true
+			return
+		}
+	}
+	if bits&svc.EntityBaselineLargeModel != 0 {
+		i, err := cls.inMessage.ReadUint16()
+		if err != nil {
+			cls.msgBadRead = true
+			return
+		}
+		es.ModelIndex = i
+	} else {
+		i, err := cls.inMessage.ReadByte()
+		if err != nil {
+			cls.msgBadRead = true
+			return
+		}
+		es.ModelIndex = uint16(i)
+	}
+	if bits&svc.EntityBaselineLargeFrame != 0 {
+		f, err := cls.inMessage.ReadUint16()
+		if err != nil {
+			cls.msgBadRead = true
+			return
+		}
+		es.Frame = f
+	} else {
+		f, err := cls.inMessage.ReadByte()
+		if err != nil {
+			cls.msgBadRead = true
+			return
+		}
+		es.Frame = uint16(f)
+	}
+
+	// colormap: no idea what this is good for. Is not really used.
+	es.ColorMap, err = cls.inMessage.ReadByte()
+	if err != nil {
+		cls.msgBadRead = true
+		return
+	}
+	es.Skin, err = cls.inMessage.ReadByte()
+	if err != nil {
+		cls.msgBadRead = true
+		return
+	}
+
+	es.Origin[0], err = cls.inMessage.ReadCoord(cl.protocolFlags)
+	if err != nil {
+		cls.msgBadRead = true
+		return
+	}
+	es.Angles[0], err = cls.inMessage.ReadAngle(cl.protocolFlags)
+	if err != nil {
+		cls.msgBadRead = true
+		return
+	}
+	es.Origin[1], err = cls.inMessage.ReadCoord(cl.protocolFlags)
+	if err != nil {
+		cls.msgBadRead = true
+		return
+	}
+	es.Angles[1], err = cls.inMessage.ReadAngle(cl.protocolFlags)
+	if err != nil {
+		cls.msgBadRead = true
+		return
+	}
+	es.Origin[2], err = cls.inMessage.ReadCoord(cl.protocolFlags)
+	if err != nil {
+		cls.msgBadRead = true
+		return
+	}
+	es.Angles[2], err = cls.inMessage.ReadAngle(cl.protocolFlags)
+	if err != nil {
+		cls.msgBadRead = true
+		return
+	}
+
+	if bits&svc.EntityBaselineAlpha != 0 {
+		es.Alpha, err = cls.inMessage.ReadByte()
+		if err != nil {
+			cls.msgBadRead = true
+			return
+		}
+	}
+
+	e.SetBaseline(es)
+}
 
 func parse3Coord() (vec.Vec3, error) {
 	x, err := cls.inMessage.ReadCoord(cl.protocolFlags)
@@ -324,7 +423,7 @@ func CL_ParseServerMessage() {
 				continue
 			}
 			// must use CL_EntityNum() to force cl.num_entities up
-			C.CL_ParseBaseline(C.CL_EntityNum(C.int(i)), 1)
+			CL_ParseBaseline(int(i), 1)
 
 		case svc.SpawnStatic:
 			C.CL_ParseStatic(1)
@@ -458,19 +557,19 @@ func CL_ParseServerMessage() {
 				}
 				C.Fog_Update(density, red, green, blue, time)
 			}
-		case svc.SpawnBaseline2: // PROTOCOL_FITZQUAKE
+		case svc.SpawnBaseline2:
 			i, err := cls.inMessage.ReadInt16()
 			if err != nil {
 				cls.msgBadRead = true
 				continue
 			}
 			// must use CL_EntityNum() to force cl.num_entities up
-			C.CL_ParseBaseline(C.CL_EntityNum(C.int(i)), 2)
+			CL_ParseBaseline(int(i), 2)
 
-		case svc.SpawnStatic2: // PROTOCOL_FITZQUAKE
+		case svc.SpawnStatic2:
 			C.CL_ParseStatic(2)
 
-		case svc.SpawnStaticSound2: // PROTOCOL_FITZQUAKE
+		case svc.SpawnStaticSound2:
 			org, err := parse3Coord()
 			if err != nil {
 				cls.msgBadRead = true

quakelib/entities.go
@@ -6,6 +6,7 @@ package quakelib
 //extern entity_t cl_viewent;
 //typedef entity_t* entityPtr;
 //entity_t* getCLEntity(int i) { return &cl_entities[i]; }
+//entity_t *CL_EntityNum(int num); // has error checks
 import "C"
 
 import (
@@ -38,4 +39,21 @@ func (e *Entity) angles() vec.Vec3 {
 
 func cl_weapon() Entity {
 	return Entity{&C.cl_viewent}
+}
+
+func CL_EntityNum(num int) *Entity {
+	return &Entity{C.CL_EntityNum(C.int(num))}
+}
+
+func (e *Entity) SetBaseline(state *EntityState) {
+	e.ptr.baseline.origin[0] = C.float(state.Origin[0])
+	e.ptr.baseline.origin[1] = C.float(state.Origin[1])
+	e.ptr.baseline.origin[2] = C.float(state.Origin[2])
+	e.ptr.baseline.angles[0] = C.float(state.Angles[0])
+	e.ptr.baseline.angles[1] = C.float(state.Angles[1])
+	e.ptr.baseline.angles[2] = C.float(state.Angles[2])
+	e.ptr.baseline.modelindex = C.ushort(state.ModelIndex)
+	e.ptr.baseline.frame = C.ushort(state.Frame)
+	e.ptr.baseline.skin = C.uchar(state.Skin)
+	e.ptr.baseline.alpha = C.uchar(state.Alpha)
 }

commit 64c5a33f7099cc23e5169abc5b877ef3d67c7f60

fix bug introduced in b59eb2dcf911508ccbf5529e345ff503c4d6b201 was not writing data to cl_static_entities but to cl_entities instead broke torches&co

quakelib/cl_parse.c
@@ -542,7 +542,7 @@ void CL_ParseStatic(int version)  // johnfitz -- added a parameter
 
   ent = &cl_static_entities[i];
   Inc_CL_num_statics();
-  CL_ParseBaseline(i, version);  // johnfitz -- added second parameter
+  CL_ParseBaselineS(i, version);  // johnfitz -- added second parameter
 
   // copy it to the current state
 

quakelib/client_parse.go
@@ -73,11 +73,20 @@ var (
 	}
 )
 
-//export CL_ParseBaseline
-func CL_ParseBaseline(i, version int) {
+//export CL_ParseBaselineS
+func CL_ParseBaselineS(i, version int) {
+	e := cl.StaticEntityNum(i)
+	CL_ParseBaseline(e, version)
+}
+
+func CL_ParseBaselineD(i, version int) {
+	e := cl.EntityNum(i)
+	CL_ParseBaseline(e, version)
+}
+
+func CL_ParseBaseline(e *Entity, version int) {
 	var err error
 	// must use CL_EntityNum() to force cl.num_entities up
-	e := cl.EntityNum(i)
 	es := &EntityState{
 		Alpha: svc.EntityAlphaDefault,
 	}
@@ -461,7 +470,7 @@ func CL_ParseServerMessage() {
 				cls.msgBadRead = true
 				continue
 			}
-			CL_ParseBaseline(int(i), 1)
+			CL_ParseBaselineD(int(i), 1)
 
 		case svc.SpawnStatic:
 			C.CL_ParseStatic(1)
@@ -625,7 +634,7 @@ func CL_ParseServerMessage() {
 				cls.msgBadRead = true
 				continue
 			}
-			CL_ParseBaseline(int(i), 2)
+			CL_ParseBaselineD(int(i), 2)
 
 		case svc.SpawnStatic2:
 			C.CL_ParseStatic(2)

quakelib/entities.go
@@ -8,6 +8,8 @@ package quakelib
 //extern entity_t cl_viewent;
 //typedef entity_t* entityPtr;
 //inline entity_t* getCLEntity(int i) { return &cl_entities[i]; }
+//extern entity_t cl_static_entities[512];
+//inline entity_t* getStaticEntity(int i) { return &cl_static_entities[i]; }
 //#endif
 import "C"
 
@@ -68,6 +70,10 @@ func CL_EntityNum(num int) C.entityPtr {
 	}
 
 	return cl.Entities(num).ptr
+}
+
+func (c *Client) StaticEntityNum(num int) *Entity {
+	return &Entity{C.getStaticEntity(C.int(num))}
 }
 
 func (c *Client) EntityNum(num int) *Entity {