9707e9c1b5dfb810b665f9393e4e0a85a70ae422
therjak <therjak@gmx.net>
2020-07-26 14:06:12 +0200

    add profiling infrastructure to main

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"flag"
 	"github.com/therjak/goquake/quakelib"
+	"log"
+	"os"
+	"runtime/pprof"
 )
 
 import (
@@ -12,7 +15,17 @@ import (
 	_ "github.com/therjak/goquake/spr"
 )
 
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+
 func main() {
 	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
 	quakelib.CallCMain()
 }
