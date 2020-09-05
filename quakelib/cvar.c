594afc7c9161fa0221dc4cd5dd8fe5aafd21aacb
therjak <therjak@gmx.net>
2020-09-05 23:34:04 +0200

    removed dead code

--- quakelib/cvar.c
+++ quakelib/cvar.c
@@ -1,6 +1,5 @@
 #include "quakedef.h"
 
-static cvar_t *cvar_vars;
 static char cvar_null_string[] = "";
 
 float Cvar_GetValue(cvar_t *variable) { return CvarGetValue(variable->id); }
@@ -14,12 +13,6 @@ const char *Cvar_GetString(cvar_t *variable) {
   strncpy(buffer, value, 2048);
   free(value);
   return buffer;
-}
-
-void Cvar_SetQuick(cvar_t *var, char *value) { CvarSetQuick(var->id, value); }
-
-void Cvar_SetValueQuick(cvar_t *var, float value) {
-  CvarSetValueQuick(var->id, value);
 }
 
 void Cvar_FakeRegister(cvar_t *v, char *name) { v->id = CvarGetID(name); }
--- quakelib/cvar.h
+++ quakelib/cvar.h
@@ -11,7 +11,5 @@ float Cvar_GetValue(cvar_t *variable);
 const char *Cvar_GetString(cvar_t *variable);
 void Cvar_FakeRegister(cvar_t *v, char *name);
 void Cvar_SetCallback(cvar_t *var, cvarcallback_t func);
-void Cvar_SetQuick(cvar_t *var, char *value);
-void Cvar_SetValueQuick(cvar_t *var, float value);
 
 #endif /* __CVAR_H__ */
