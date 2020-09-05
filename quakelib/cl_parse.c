a4725fc183a06b7cf325dc8a7faa147c5f4e372a
therjak <therjak@gmx.net>
2020-09-05 23:25:00 +0200

    removed dead code

--- quakelib/cl_parse.c
+++ quakelib/cl_parse.c
@@ -70,24 +70,6 @@ void CL_ParseBeam(const char *name, int ent, float s1, float s2, float s3,
     }
   }
 }
-/*
-=================
-CL_NewTempEntity
-=================
-*/
-/*
-entity_t *CL_NewTempEntity(void) {
-  entity_t *ent;
-
-  if (VisibleEntitiesNum() >= MAX_VISEDICTS) return NULL;
-  if (num_temp_entities >= MAX_TEMP_ENTITIES) return NULL;
-  ent = &cl_temp_entities[num_temp_entities];
-  memset(ent, 0, sizeof(*ent));
-  num_temp_entities++;
-  AddVisibleEntity(ent);
-
-  return ent;
-}*/
 
 /*
 =================
