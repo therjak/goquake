c180c7ddac30bb25c317e0a00d619e523645c27d
therjak <therjak@gmx.net>
2020-10-17 17:33:52 +0200

    some more preparation for shader based dynamic light rendering

--- quakelib/renderer.go
+++ quakelib/renderer.go
@@ -9,6 +9,7 @@ import (
 	"github.com/therjak/goquake/math/vec"
 	"github.com/therjak/goquake/model"
 	"github.com/therjak/goquake/progs"
+	"log"
 )
 
 type fPlane struct {
@@ -184,11 +185,6 @@ func (r *qRenderer) DrawWeaponModel() {
 	gl.DepthRange(0, 1)
 }
 
-func drawVertex(v vec.Vec3) {
-	// dummy for gl.Vertex3fv
-	// probably something similar to qParticleDrawer.Draw
-}
-
 var circleDrawer *qCircleDrawer
 
 type qCircleDrawer struct {
@@ -197,6 +193,13 @@ type qCircleDrawer struct {
 	prog       *glh.Program
 	projection int32
 	modelview  int32
+}
+
+type qCircle struct {
+	origin     vec.Vec3
+	radius     float32
+	innerColor [3]float32
+	outerColor [3]float32
 }
 
 func newCircleDrawProgram() (*glh.Program, error) {
@@ -215,6 +218,122 @@ func newCircleDrawer() *qCircleDrawer {
 	d.projection = d.prog.GetUniformLocation("projection")
 	d.modelview = d.prog.GetUniformLocation("modelview")
 	return d
+}
+
+func (cd *qCircleDrawer) Draw(cs []qCircle, viewOrigin, viewForward, viewRight, viewUp vec.Vec3) {
+	// viewport(0,0,screen.width, screen.height)
+
+	gl.DepthMask(false)
+	gl.Disable(gl.TEXTURE_2D)
+
+	//glShadeModel(GL_SMOOTH);
+	gl.Enable(gl.BLEND)
+	gl.BlendFunc(gl.ONE, gl.ONE)
+
+	projection := [16]float32{
+		1, 0, 0, 0,
+		0, 1, 0, 0,
+		0, 0, 1, 0,
+		0, 0, 0, 1,
+	}
+	modelview := [16]float32{
+		1, 0, 0, 0,
+		0, 1, 0, 0,
+		0, 0, 1, 0,
+		0, 0, 0, 1,
+	}
+	// NOTE about the matrix element order:
+	// [00][04][08][12]
+	// [01][05][09][13]
+	// [02][06][10][14]
+	// [03][07][11][15]
+
+	// projection is a matrix based on
+	// fov, screen.width/screen.height, nearclip, farclip
+	// fov = tan(fovx * pi / 360), checked
+	// aspect ratio is correct
+	// fov 0 0 0
+	// 0 aspect, 0,0
+	// ....
+	// all values are coming from
+	// fovy = atan(0.75/(sh/sw)*tan(fovx/360pi)) * 360/pi
+	// xmax = 4 * tan(fovx * pi / 360)
+	// ymax = 4 * tan(fovy * pi / 360)
+	// glFrustum(-xmax, xmax, -ymax, ymax, 4, cvars.gl_farclip)
+	// -> 8/-2xmax, 0, 0, 0
+	//    0, 8/-2ymax, 0, 0
+	//    0, 0, -(far+4)/(far-4), -(2*far*4)/(far-4)
+	//    0, 0, -1, 0
+	gl.GetFloatv(0x0BA7, &projection[0])
+
+	// modelview should be
+	// viewright.x,viewUp.x,-viewForward.x,0
+	// viewright.y,viewUp.y,-viewForward.y,0
+	// viewright.z,viewUp.z,-viewForward.z,0
+	// ?| ?| ?| 1
+	// it get set by
+	// glRotatef(-90, 1, 0, 0)
+	// glRotatef(90, 0, 0, 1)
+	// glRotatef(-viewangles[2], 1, 0, 0)
+	// glRotatef(-viewangles[0], 0, 1, 0)
+	// glRotatef(-viewangles[1], 0, 0, 1)
+	// glTranslate(-vieworg[0], -vieworg[1], -vieworg[2])
+	gl.GetFloatv(0x0BA6, &modelview[0])
+	/*
+		log.Printf("mv: %v", modelview)
+		log.Printf("ex: %v, %v, %v, %v, %v",
+			qRefreshRect.viewOrg,
+			qRefreshRect.viewForward,
+			qRefreshRect.viewRight,
+			qRefreshRect.viewUp,
+			qRefreshRect.viewAngles)
+	*/
+	if false {
+		log.Printf("something")
+	}
+
+	cd.prog.Use()
+	cd.vao.Bind()
+	cd.vbo.Bind(gl.ARRAY_BUFFER)
+	// TODO: remove this allocation
+	data := make([]float32, 0, len(cs)*(3+1+3+3))
+	for _, c := range cs {
+		data = append(data,
+			c.origin[0], c.origin[1], c.origin[2],
+			c.radius,
+			c.innerColor[0], c.innerColor[1], c.innerColor[2],
+			c.outerColor[0], c.outerColor[1], c.outerColor[2])
+	}
+	gl.BufferData(gl.ARRAY_BUFFER, 4*len(data), gl.Ptr(data), gl.STATIC_DRAW)
+
+	gl.EnableVertexAttribArray(0)
+	gl.EnableVertexAttribArray(1)
+	gl.EnableVertexAttribArray(2)
+	gl.EnableVertexAttribArray(3)
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 4*8, gl.PtrOffset(0))
+	gl.VertexAttribPointer(1, 1, gl.FLOAT, false, 4*8, gl.PtrOffset(3*4))
+	gl.VertexAttribPointer(2, 3, gl.FLOAT, false, 4*8, gl.PtrOffset(4*4))
+	gl.VertexAttribPointer(3, 3, gl.FLOAT, false, 4*8, gl.PtrOffset(7*4))
+
+	gl.UniformMatrix4fv(cd.projection, 1, false, &projection[0])
+	gl.UniformMatrix4fv(cd.modelview, 1, false, &modelview[0])
+
+	// gl.DrawArrays(gl.TRIANGLES, 0, int32(len(cs)))
+
+	gl.DisableVertexAttribArray(0)
+	gl.DisableVertexAttribArray(1)
+	gl.DisableVertexAttribArray(2)
+	gl.DisableVertexAttribArray(3)
+
+	gl.Disable(gl.BLEND)
+	gl.Enable(gl.TEXTURE_2D)
+	gl.BlendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA)
+	gl.DepthMask(true)
+}
+
+func drawVertex(v vec.Vec3) {
+	// dummy for gl.Vertex3fv
+	// probably something similar to qParticleDrawer.Draw
 }
 
 func (r *qRenderer) RenderDynamicLight(l *DynamicLight) {
@@ -270,27 +389,43 @@ func (r *qRenderer) RenderDynamicLights() {
 		// TODO(therjak): disabling flashblend is broken since transparent console
 		return
 	}
+	if circleDrawer == nil {
+		circleDrawer = newCircleDrawer()
+	}
 
 	r.lightFrameCount++
-
-	gl.DepthMask(false)
-	gl.Disable(gl.TEXTURE_2D)
-
-	//glShadeModel(GL_SMOOTH);
-	gl.Enable(gl.BLEND)
-	gl.BlendFunc(gl.ONE, gl.ONE)
+	// TODO: remove this allociation
+	cs := make([]qCircle, 0, len(cl.dynamicLights))
 	for i := range cl.dynamicLights {
 		dl := &cl.dynamicLights[i]
 		if dl.DieTime < cl.time || dl.Radius == 0 {
 			continue
 		}
-		r.RenderDynamicLight(dl)
+		// TODO: why do we need this scaling of radius. can the radius be
+		// 'right' from the start?
+		rad := dl.Radius * 0.35
+		d := vec.Sub(dl.Origin, qRefreshRect.viewOrg)
+		if d.Length() < rad {
+			// view is inside the dynamic light
+			view.addLightBlend(1, 0.5, 0, dl.Radius*0.0003)
+			continue
+		}
+
+		cs = append(cs, qCircle{
+			origin:     dl.Origin,
+			radius:     rad,
+			innerColor: [3]float32{0.2, 0.1, 0.0},
+			outerColor: [3]float32{0, 0, 0},
+		})
+		// r.RenderDynamicLight(dl)
 	}
+	if len(cs) == 0 {
+		return
+	}
+	circleDrawer.Draw(cs,
+		qRefreshRect.viewOrg,
+		qRefreshRect.viewForward,
+		qRefreshRect.viewRight,
+		qRefreshRect.viewUp)
 	// glColor3f(1, 1, 1);
-
-	gl.Disable(gl.BLEND)
-	gl.Enable(gl.TEXTURE_2D)
-	gl.BlendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA)
-	gl.DepthMask(true)
-
 }
