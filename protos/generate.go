29f9de1041a7ca4176b44872a3f6e4a0c2789cda
therjak <therjak@gmx.net>
2024-12-24 11:46:51 +0100

    migrate to HYBRID proto api

--- protos/generate.go
+++ protos/generate.go
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-//go:generate  protoc --go_out=. --go_opt=paths=source_relative savegame.proto
-//go:generate  protoc --go_out=. --go_opt=paths=source_relative history.proto
-//go:generate  protoc --go_out=. --go_opt=paths=source_relative client_message.proto
-//go:generate  protoc --go_out=. --go_opt=paths=source_relative server_message.proto
+//go:generate  protoc -I/usr/local/include -I. --go_out=. --go_opt=paths=source_relative savegame.proto
+//go:generate  protoc -I/usr/local/include -I. --go_out=. --go_opt=paths=source_relative history.proto
+//go:generate  protoc -I/usr/local/include -I. --go_out=. --go_opt=paths=source_relative client_message.proto
+//go:generate  protoc -I/usr/local/include -I. --go_out=. --go_opt=paths=source_relative server_message.proto
 package protos
